1bbf96795203874c33b15f17337c93a53645d5a3
BenZeltser <92685838+BenZeltser@users.noreply.github.com>
2021-10-22 20:30:04 +0300

    Changes by Tomer

--- Assignments/Ex0/src/ex0/algo/ElevatorAlgoAns.java
+++ Assignments/Ex0/src/ex0/algo/ElevatorAlgoAns.java
@@ -6,13 +6,14 @@ import ex0.Elevator;
 import java.util.Iterator;
 
 public class ElevatorAlgoAns implements ElevatorAlgo, Elevator, CallForElevator {
-        public static final int UP=1, DOWN=-1, LEVEL=0 ;
-        private int _direction = UP;
-        private Building _building;
-        public ElevatorAlgoAns(Building b) {
-            _building = b;
-            _direction = UP;
-        }
+    public static final int UP = 1, DOWN = -1, LEVEL = 0;
+    private int _direction = UP;
+    private Building _building;
+
+    public ElevatorAlgoAns(Building b) {
+        _building = b;
+        _direction = UP;
+    }
 
     @Override
     public Building getBuilding() {
@@ -21,42 +22,31 @@ public class ElevatorAlgoAns implements ElevatorAlgo, Elevator, CallForElevator 
 
     @Override
     public String algoName() {
-        return"Ex0_OOP_Smart_Elevator_ALgo";
+        return "Ex0_OOP_Smart_Elevator_ALgo";
     }
 
     @Override
     public int allocateAnElevator(CallForElevator c) {
-
+        return 0;
     }
 
     @Override
     public void cmdElevator(int elev) {
 
     }
-    private int get_direction(Elevator elevator)
-    { return 
-    }
-}
 
-    private double dist(int src, int elev) {
+    //this function calculates the time a resting elevator will take to reach the source of the call
+    private double distRest(int src, int elev) {
         double ans = -1;
         Elevator thisElev = this._building.getElevetor(elev);
         int pos = thisElev.getPos();
         double speed = thisElev.getSpeed();
         int min = this._building.minFloor(), max = this._building.maxFloor();
-        double up2down = (max - min) * speed;
-        if (elev % 2 == 1) { // up
-            if (pos <= src) {
-                ans = (src - pos);
-            } else {
-                ans = ((max - pos) + (pos - min)) + up2down;
-            }
-        } else {
-            if (pos >= src) {
-                ans = (pos - src);
-            } else {
-                ans = ((max - pos) + (pos - min));
-            }
+        if(thisElev.getState()==0){
+            if (src>pos)
+                ans=(src-pos)*speed;
+            else
+                ans=(pos-src)*speed;
         }
         return ans;
     }
@@ -146,4 +136,5 @@ public class ElevatorAlgoAns implements ElevatorAlgo, Elevator, CallForElevator 
     public int getID() {
         return 0;
     }
-}
+    }
+*/
