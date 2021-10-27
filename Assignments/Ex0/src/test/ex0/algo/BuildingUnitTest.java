ea6e9791cb2aeaafe7c1759f7151c85812cc83a1
BenZeltser <92685838+BenZeltser@users.noreply.github.com>
2021-10-27 22:42:29 +0300

    Excellent Progress

---
+++ Assignments/Ex0/src/test/ex0/algo/BuildingUnitTest.java
@@ -0,0 +1,195 @@
+package test.ex0.algo; 
+
+import org.testng.annotations.Test;
+import org.junit.Before; 
+import org.junit.After; 
+
+/** 
+* BuildingUnit Tester. 
+* 
+* @author <Authors name> 
+* @since <pre>рхїз 27, 2021</pre> 
+* @version 1.0 
+*/ 
+public class BuildingUnitTest { 
+
+@Before
+public void before() throws Exception { 
+} 
+
+@After
+public void after() throws Exception { 
+} 
+
+/** 
+* 
+* Method: getBuildingName() 
+* 
+*/ 
+@Test
+public void testGetBuildingName() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: minFloor() 
+* 
+*/ 
+@Test
+public void testMinFloor() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: maxFloor() 
+* 
+*/ 
+@Test
+public void testMaxFloor() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: numberOfElevetors() 
+* 
+*/ 
+@Test
+public void testNumberOfElevetors() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getElevetor(int i) 
+* 
+*/ 
+@Test
+public void testGetElevetor() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+
+/** 
+* 
+* Method: setMinFloor(int minFloor) 
+* 
+*/ 
+@Test
+public void testSetMinFloor() throws Exception { 
+//TODO: Test goes here... 
+/* 
+try { 
+   Method method = BuildingUnit.getClass().getMethod("setMinFloor", int.class); 
+   method.setAccessible(true); 
+   method.invoke(<Object>, <Parameters>); 
+} catch(NoSuchMethodException e) { 
+} catch(IllegalAccessException e) { 
+} catch(InvocationTargetException e) { 
+} 
+*/ 
+} 
+
+/** 
+* 
+* Method: setMaxFloor(int maxFloor) 
+* 
+*/ 
+@Test
+public void testSetMaxFloor() throws Exception { 
+//TODO: Test goes here... 
+/* 
+try { 
+   Method method = BuildingUnit.getClass().getMethod("setMaxFloor", int.class); 
+   method.setAccessible(true); 
+   method.invoke(<Object>, <Parameters>); 
+} catch(NoSuchMethodException e) { 
+} catch(IllegalAccessException e) { 
+} catch(InvocationTargetException e) { 
+} 
+*/ 
+} 
+
+/** 
+* 
+* Method: getNumberOfElevetors() 
+* 
+*/ 
+@Test
+public void testGetNumberOfElevetors() throws Exception { 
+//TODO: Test goes here... 
+/* 
+try { 
+   Method method = BuildingUnit.getClass().getMethod("getNumberOfElevetors"); 
+   method.setAccessible(true); 
+   method.invoke(<Object>, <Parameters>); 
+} catch(NoSuchMethodException e) { 
+} catch(IllegalAccessException e) { 
+} catch(InvocationTargetException e) { 
+} 
+*/ 
+} 
+
+/** 
+* 
+* Method: setNumberOfElevetors(int numberOfElevetors) 
+* 
+*/ 
+@Test
+public void testSetNumberOfElevetors() throws Exception { 
+//TODO: Test goes here... 
+/* 
+try { 
+   Method method = BuildingUnit.getClass().getMethod("setNumberOfElevetors", int.class); 
+   method.setAccessible(true); 
+   method.invoke(<Object>, <Parameters>); 
+} catch(NoSuchMethodException e) { 
+} catch(IllegalAccessException e) { 
+} catch(InvocationTargetException e) { 
+} 
+*/ 
+} 
+
+/** 
+* 
+* Method: getElevators() 
+* 
+*/ 
+@Test
+public void testGetElevators() throws Exception { 
+//TODO: Test goes here... 
+/* 
+try { 
+   Method method = BuildingUnit.getClass().getMethod("getElevators"); 
+   method.setAccessible(true); 
+   method.invoke(<Object>, <Parameters>); 
+} catch(NoSuchMethodException e) { 
+} catch(IllegalAccessException e) { 
+} catch(InvocationTargetException e) { 
+} 
+*/ 
+} 
+
+/** 
+* 
+* Method: setElevators(ArrayList<ElevatorUnit> elevators) 
+* 
+*/ 
+@Test
+public void testSetElevators() throws Exception { 
+//TODO: Test goes here... 
+/* 
+try { 
+   Method method = BuildingUnit.getClass().getMethod("setElevators", ArrayList<ElevatorUnit>.class); 
+   method.setAccessible(true); 
+   method.invoke(<Object>, <Parameters>); 
+} catch(NoSuchMethodException e) { 
+} catch(IllegalAccessException e) { 
+} catch(InvocationTargetException e) { 
+} 
+*/ 
+} 
+
+} 
